3de9f11a5d9d08ac602f6c8b32d8fb40ac4e599d
alecordix <andrea.lecordix@gmail.com>
2020-04-03 16:42:31 +0200

    Tout sur le même fichier

--- tp1/exercices.py
+++ tp1/exercices.py
@@ -6,43 +6,43 @@ print(type(2.5))
 
 #Exercice 2
 
-for i in range (20) :
-    if (i%2 == 0):
-        print("Le chiffre",i,"est pair !")
-        i += 1
-    else:
-        i += 1
+for i in range (20) : # pour i allant de 0 à 20
+    if (i%2 == 0): # si i est pair
+        print("Le chiffre",i,"est pair !") # écrire "Le chiffre i est pair"
+        i += 1 # ajouter 1 à i
+    else: #sinon
+        i += 1 # ajouter 1 à i
 
-while i <= 20:
-    if (i%2 == 0):
+while i <= 20: # tant que i est inférieur ou égal à 20
+    if (i%2 == 0): # si i est pair
         print("Le chiffre",i,"est pair !")
-        i += 1
-    else:
-        i += 1
+        i += 1 # ajouter 1 à i
+    else: #sinon
+        i += 1 # ajouter 1 à i
 
 #Exercice 3
 
-n=int(input("Saisissez une valeur positive : "))
-s=0
-for x in range (n):
-     if(x%2 != 0):
-         print(x)
-         s=s+x
-     else:
-         print("faux")
-print("La somme des impairs est", s)
+n=int(input("Saisissez une valeur positive : ")) #demander à l'utilisateur de rentrer un entier n
+s=0 # initialiser s à 0
+for x in range (n): # pour i allant de 0 à n
+     if(x%2 != 0): # si x modulo 2 est différent de 0 (impair)
+         print(x) # écrire x
+         s=s+x # ajouter x à s
+     else: # sinon
+         print("faux") # écrire "faux"
+print("La somme des impairs est", s) #écrire "La somme des impairs est s"
 
 
 #Exercice 4
 
 i = 1
-n = int(input("Veuillez saisir une valeur comprise entre 1 et 100 inclus : "))
-while(n <= 0 or n >= 101):
-    n=int(input("Veuillez saisir une valeur comprise entre 1 et 100 inclus : "))
+n = int(input("Veuillez saisir une valeur comprise entre 1 et 100 inclus : ")) # demander à l'utilisateur d'entrer un entier n compris entre 1 et 100 inclus
+while(n <= 0 or n >= 101): # tant que n est inférieur ou égal à 0 OU supérieur ou égal à 100 :
+    n=int(input("Veuillez saisir une valeur comprise entre 1 et 100 inclus : ")) # redemander à l'utilisateur d'entrer un entier n compris entre 1 et 100 inclus
 
-while (i >= 1 and i <= n):
-    print(i)
-    i = i + 1
+while (i >= 1 and i <= n): # tant que i est supérieur ou égal à 1 ET inférieur ou égal à n :
+    print(i)# écrire i
+    i = i + 1 # ajouter 1 à i
 print("STOP ! C'est fini !")
 
 #Exercice 5
