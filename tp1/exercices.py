b44079993c2a33cede732d0350dee738fcd32430
alecordix <andrea.lecordix@gmail.com>
2020-04-03 15:35:11 +0200

    Tout sur le même fichier

--- tp1/exercices.py
+++ tp1/exercices.py
@@ -22,7 +22,15 @@ while i <= 20:
 
 #Exercice 3
 
-
+n=int(input("Saisissez une valeur positive : "))
+s=0
+for x in range (n):
+     if(x%2 != 0):
+         print(x)
+         s=s+x
+     else:
+         print("faux")
+print("La somme des impairs est", s)
 
 
 #Exercice 4
