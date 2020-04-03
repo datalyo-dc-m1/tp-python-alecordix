266a52dd5ca3e3015cf0a0b46f92328f0a806457
alecordix <andrea.lecordix@gmail.com>
2020-04-03 14:47:49 +0200

    exercice 3

---
+++ tp1/exercice3.py
@@ -0,0 +1,15 @@
+#Exercice 3
+
+for i in range (20) :
+    if (i%2 == 0):
+        print("Le chiffre",i,"est pair !")
+        i += 1
+    else:
+        i += 1
+
+while i <= 20:
+    if (i%2 == 0):
+        print("Le chiffre",i,"est pair !")
+        i += 1
+    else:
+        i += 1
