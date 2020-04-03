65b16d4cf69d6052256d47362f0b3d29ca5e958c
alecordix <andrea.lecordix@gmail.com>
2020-04-03 15:23:37 +0200

    Tout sur le même fichier

--- tp1/exercices.py
+++ tp1/exercices.py
@@ -36,3 +36,36 @@ while (i >= 1 and i <= n):
     print(i)
     i = i + 1
 print("STOP ! C'est fini !")
+
+#Exercice 5
+
+a = int(input("Donnez le premier nombre : "))
+print(a)
+b = int(input("Donnez le premier nombre : "))
+print(b)
+op = input("Donnez le symbole de l'opération: ")
+print(op)
+print()
+if(op=='+'):
+    print('Le résultat est', a+b)
+if(op=='-'):
+    print('Le résultat est', a-b)
+if(op=='*'):
+    print('Le résultat est', a*b)
+if (op == '/'):
+    if(b==0):
+        print("Opération impossible")
+    else:
+        print('Le résultat est', a/b)
+if (op!='+' and op!='-' and op!='*' and op!='/'):
+    print("Opération inexistante. Veuillez choisir un opérateur ( + , - , * , /)")
+
+
+
+
+
+
+
+
+
+
