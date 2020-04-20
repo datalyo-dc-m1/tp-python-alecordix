47bd74629ec525d04832feecfbf525b7f70d3b81
alecordix <andrea.lecordix@gmail.com>
2020-04-20 11:32:34 +0200

    POO

--- tp2/monkey.py
+++ tp2/monkey.py
@@ -1,17 +1,17 @@
 class Monkey:
-    def __init__(self, name, banana):
+    def __init__(self, name, banane):
         self.name = name
-        self.banana = banana
+        self.banane = banane
     def eat(self):
-        print('Un singé nommé', self.name, 'mange une banane de couleur', self.banana.color)
+        print('Un singé nommé', self.name, 'mange une banane de couleur', self.banane.color)
 
-class Banana:
+class Banane:
     def __init__(self, color):
         self.color = color
 
 
-banane_verte = Banana('verte')
-banane_jaune = Banana('jaune')
+banane_verte = Banane('verte')
+banane_jaune = Banane('jaune')
 Pierre = Monkey("Pierre", banane_jaune)
 Marie = Monkey("Marie", banane_verte)
 
